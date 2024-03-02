15c871246c85e1e70e69776efc7d84cd612059c5
RaulStanciu1 <104789551+RaulStanciu1@users.noreply.github.com>
2024-03-02 08:07:36 +0200

    Added Initial JavaFX Files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.ctestwizard {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.ctestwizard to javafx.fxml;
+    exports com.ctestwizard;
+    exports com.ctestwizard.controller;
+    opens com.ctestwizard.controller to javafx.fxml;
+}
